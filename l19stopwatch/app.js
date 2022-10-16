de1240cff4d02157f321374281f1961b03ddb67e
Adam26114 <zakato26114@gmail.com>
2022-10-16 23:56:33 +0630

    10 Oct 2022

--- l19stopwatch/app.js
+++ l19stopwatch/app.js
@@ -1,8 +1,11 @@
-const getdisplay = document.querySelector(".display");
+// const getdisplay = document.querySelector(".display");
+const getdisplay = document.querySelectorAll(".display");
 const getstartbtn = document.querySelector(".start");
 const getpausebtn = document.querySelector(".pause")
-const getresetbtn = document.querySelector(".reset")
-
+const getresetbtn = document.querySelector(".reset");
+const getdlpsec = document.getElementById( 'dlpsecond' ) ;
+const getdlpmlsec = document.getElementById( 'dlpmillisec' ) ;
+const getmodebtn = document.querySelector(".mode-btn");
 
 // var hours        = 0,
 //     minutes      = 0,
@@ -12,57 +15,198 @@ const getresetbtn = document.querySelector(".reset")
 var [hours,minutes,seconds,milliseconds] = [0,0,0,0];
 // console.log(seconds);
 
-getstartbtn.addEventListener('click',starttime);
-getpausebtn.addEventListener('click',pausetime);
-getresetbtn.addEventListener('click',resettime);
-
 var setivdisplay ;
 
-function starttime(){
-    // console.log("hey i am start time");
+// ---------------------------------------------------------
 
-    setivdisplay = setInterval(showdisplay,100);
 
+// getstartbtn.addEventListener('click',starttime);
+// getpausebtn.addEventListener('click',pausetime);
+// getresetbtn.addEventListener('click',resettime);
+
+
+// function starttime(){
+//     // console.log("hey i am start time");
+
+//     clearInterval(setivdisplay);
+
+//     setivdisplay = setInterval(showdisplay,10);
+
+// }
+
+// function pausetime(){
+//     // console.log("hey i am pause time");
+
+//     clearInterval(setivdisplay);
+// }
+
+// function resettime(){
+//     // console.log("hey i am reset time");
+//     clearInterval(setivdisplay);
+//     [hours,minutes,seconds,milliseconds] = [0,0,0,0];
+//     getdisplay.innerHTML = "00 : 00 : 00 : 000"
+// }
+
+// function showdisplay(){
+//     // console.log("hay hay hay. hee hee");
+
+ 
+//     milliseconds += 10;
+
+//     if(milliseconds === 1000){
+//         milliseconds = 0;
+
+//         seconds++;
+
+//         if(seconds === 60){
+
+//             seconds = 0;
+
+//             minutes++;
+
+//             if(minutes === 60){
+
+//                 minutes = 0;
+
+//                 hours++;
+//             }
+
+//         }
+//     }
+
+    
+//     // console.log(milliseconds);
+//     // console.log(seconds);
+//     // console.log(minutes);
+
+//     let h = hours < 10 ? "0"+hours : hours;
+//     let m = minutes < 10 ? "0"+minutes : minutes;
+//     let s = seconds < 10 ? "0"+seconds : seconds;
+//     let ms = milliseconds < 10 ? "00"+minutes : milliseconds < 100 ? "0"+milliseconds : milliseconds;
+
+//     getdisplay.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+// }
+
+
+// ---------------------------------------------------------
+
+var idx = 0 ;
+
+
+getdisplay[idx].style.display = "block"
+
+getmodebtn.onclick = function(){
+    
+    getdisplay[idx].style.display = "none";
+    
+    idx++;
+    
+    
+    if(idx > 1){
+        idx = 0;
+    }
+    
+
+    getdisplay[idx].style.display = "block"
+    // console.log(idx);
+
+    getmodebtn.innerHTML = `Mode ${idx+1}`
 }
 
-function pausetime(){
-    // console.log("hey i am pause time");
+getstartbtn.onclick = function(){
+    clearInterval(setivdisplay);
+
+    setivdisplay = setInterval(showdisplay,10);
+}
+
+getpausebtn.onclick = function(){
 
     clearInterval(setivdisplay);
+
 }
 
-function resettime(){
-    // console.log("hey i am reset time");
+getresetbtn.onclick = function(){
+
+    if(idx === 0 ){
+        clearInterval(setivdisplay);
+        [hours,minutes,seconds,milliseconds] = [0,0,0,0];
+        getdisplay[idx].innerHTML = "00 : 00 : 00 : 000"
+    }else{
+        
+        clearInterval(setivdisplay);
+        milliseconds = "00";
+        seconds = "00";
+        getdlpmlsec.innerHTML = milliseconds;
+        getdlpsec.innerHTML = seconds;
+    }
+
+
+
 }
 
 function showdisplay(){
-    // console.log("hay hay hay. hee hee");
 
- 
-    milliseconds += 10;
+    if(idx === 0){
 
-    if(milliseconds === 1000){
-        milliseconds = 0;
+        milliseconds += 10;
 
-        seconds++;
+        if(milliseconds === 1000){
+            milliseconds = 0;
 
-        if(seconds === 60){
+            seconds++;
 
-            seconds = 0;
+            if(seconds === 60){
 
-            minutes++;
+                seconds = 0;
 
-            if(minutes === 60){
+                minutes++;
 
-                minutes = 0;
+                if(minutes === 60){
 
-                hours++;
+                    minutes = 0;
+
+                    hours++;
+                }
+
             }
-
         }
-    }
 
-    console.log(milliseconds);
-    console.log(seconds);
-    console.log(minutes);
+        
+        // console.log(milliseconds);
+        // console.log(seconds);
+        // console.log(minutes);
+
+        let h = hours < 10 ? "0"+hours : hours;
+        let m = minutes < 10 ? "0"+minutes : minutes;
+        let s = seconds < 10 ? "0"+seconds : seconds;
+        let ms = milliseconds < 10 ? "00"+minutes : milliseconds < 100 ? "0"+milliseconds : milliseconds;
+
+        getdisplay[idx].innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+
+    }else{
+        
+        milliseconds++;
+
+        // console.log(milliseconds);
+
+        if(milliseconds <= 9){
+            getdlpmlsec.innerHTML = "0"+milliseconds;
+        }
+        
+        if(milliseconds > 9){
+            getdlpmlsec.innerHTML = milliseconds;
+        }
+        
+        if(milliseconds > 99){
+            milliseconds = 0;
+            seconds++;
+            getdlpmlsec.innerHTML = "0" + 0;
+            getdlpsec.innerHTML = "0"+seconds;
+        }
+
+        if(seconds > 9){
+            getdlpsec.innerHTML = seconds;
+        }
+    
+    }
 }
