8a46aebd8f6a75bb039f718df4accf43be99de2b
Adam26114 <zakato26114@gmail.com>
2022-12-20 08:44:16 +0630

    20 Dec 2022

---
+++ l24videoplayer/app.js
@@ -0,0 +1,11 @@
+// Get Ui
+
+const getvideoscreen = document.getElementById('videoscreen');
+ 
+const playbtn = document.getElementById('play');
+const prevbtn = document.getElementById('prev');
+const nextbtn = document.getElementById('next');
+const stopbtn = document.getElementById('stop');
+
+const progress = document.getElementById('progress');
+const getfullscreen = document.getElementById('fullscreen');
