dfcaeb0c412741a08c7000e4ee90259b711102d9
Adam26114 <zakato26114@gmail.com>
2022-12-27 09:04:44 +0630

    27 Dec 2022

--- l24videoplayer/app.js
+++ l24videoplayer/app.js
@@ -1,166 +1,279 @@
-// Get Ui
-
+// UI
 const getvideoscreen = document.getElementById('videoscreen');
- 
+
 const playbtn = document.getElementById('play');
 const prevbtn = document.getElementById('prev');
 const nextbtn = document.getElementById('next');
 const stopbtn = document.getElementById('stop');
 
-const progress = document.getElementById('progress');
+// For Range
+// const progress = document.getElementById('progress');
 const getdisplaytime = document.getElementById('displaytime')
 const getfullscreen = document.getElementById('fullscreen');
-const getcontainer = document.querySelector('.container');
-const getopnfullscreeen = document.getElementById('opnfullscreen');
-const getclsfullscreen = document.getElementById('clsfullscreen');
 
-const videos = ['simplevideo1','simplevideo2']
+const getcontainer = document.querySelector('.container');
+const getopnfullscreen = document.querySelector('.openfullscreen');
+const getclsfullscreen = document.querySelector('.closefullscreen');
+
+const gettitle = document.getElementById('title');
+
+//For Progress Container
+
+const getprogressctn = document.getElementById('progress-container')
+const progress = document.getElementById('progress');
+
+const videos = ['samplevideo1', 'samplevideo2'];
 
 let curridx = 0;
 
 loadvideo(videos[curridx]);
 
-function loadvideo(vdo){
-    getfullscreen.src = `./source/${vdo}.mp4`
+function loadvideo(vdo) {
+    getvideoscreen.src = `./source/${vdo}.mp4`;
+	gettitle.textContent = vdo;
+	
 }
 
-function playvdo(){
+function playvdo() {
 
     playbtn.querySelector('i.fas').classList.remove('fa-play');
     playbtn.querySelector('i.fas').classList.add('fa-pause');
 
-    // play() method came from video api
+
+    // play() methd came from video api
     getvideoscreen.play();
 }
 
-function pausevdo(){
+function pausevdo() {
 
-    playbtn.querySelector('i.fas').remove('fa-pause');
-    playbtn.querySelector('i.fas').add('fa-play');
-    
-    // pasue() method came from video api
-    getvideoscreen.pasue();
+    playbtn.querySelector('i.fas').classList.remove('fa-pause');
+    playbtn.querySelector('i.fas').classList.add('fa-play');
+
+    // pause() methd came from video api
+    getvideoscreen.pause();
 }
 
-function playpausevdo(){
-    //pasued keyword came from video api
-    if(getvideoscreen.paused){
-        // getvideoscreen.play();
+function playpausevdo() {
+    //  paused keyword came from video api
+    if (getvideoscreen.paused) {
+        //getvideoscreen.play();
         playvdo();
-    }else{
-        // getvideoscreen.pause();
+    } else {
+        //getvideoscreen.pause();
         pausevdo();
     }
+
 }
 
-function nextvdo(){
+
+function nextvdo() {
     curridx++;
 
-    if(curridx > videos.length - 1){
+    if (curridx > videos.length - 1) {
         curridx = 0;
     }
 
     // console.log(curridx);
-
     loadvideo(videos[curridx]);
-
     playvdo();
+
 }
 
-function previousvdo(){
-    curridx -= 1 ;
+function prevvdo() {
+    curridx--;
 
-    if(curridx < 0){
+    if (curridx < 0) {
         curridx = videos.length - 1;
     }
 
-    // console.log(curridx);
-
+    //  console.log(curridx);
     loadvideo(videos[curridx]);
     playvdo();
 }
 
-function updateprogress(){
-    // console.log('hay');
-    // currentTime came from video api
+function stopvideo(){
+    getvideoscreen.currentTime = 0;
+    // getvideoscreen.pause();
+    pausevdo();
+}
+
+function updateprogress(e){
+
+    // Method 2 m1
+    // console.log(e.target);
+    // console.log(e.srcElement);
+    // console.log(this)
+
+    // const currentTime = e.target.currentTime;
+    // const duration = e.target.duration;
+    // console.log(currentTime,duration);
+
+    // M2
+    // const {currentTime} = e.target;
+    // const {duration} = e.target;
+    // console.log(currentTime,duration);
+
+
+    // Method 2 m3
+    // const {currentTime,duration} = e.target
+    // console.log(currentTime,duration);
+
+    // Method 2 m4 
+    const [currentTime,duration] = [e.target.currentTime,e.srcElement.duration]
+    // console.log(currentTime,duration);
+
+    // console.log('hey')
+    // Method 1
+    //currentTime came from video api 
     // console.log(getvideoscreen.currentTime);
+
+    // duration came from video api 
     // console.log(getvideoscreen.duration);
+    // console.log((getvideoscreen.currentTime / getvideoscreen.duration) * 100)
 
-    // console.log(getvideoscreen.currentTime / getvideoscreen.duration) * 100;
 
-    progress.value = (getvideoscreen.currentTime / getvideoscreen.duration) * 100;
+	//For Range
+    // if(getvideoscreen.currentTime === 0){
+    //     progress.value = 0
+    // }else{
+    //     // progress.value = (getvideoscreen.currentTime / getvideoscreen.duration) * 100
 
-    let getmin = Math.floor(getvideoscreen.currentTime / 60);
-    // console.log(getmin);
+    //     progress.value = (currentTime / duration) * 100
 
-    if(getmin < 10){
-        // getmins = '0'+getmins;
-        getmins = '0'+String(getmins);
-    }
+    // }
+
+
+	// For Progress Container
+	if(getvideoscreen.currentTime === 0){
+		progress.style.width = '0%'
+	}else{
+		const progresspercnent = (currentTime / duration) * 100;
+		progress.style.width = `${progresspercnent}%`;
+	}
+
+
+    // progress.value = (getvideoscreen.currentTime / getvideoscreen.duration) * 100
+
+
+
+    let getmins = Math.floor(getvideoscreen.currentTime/60);
+    // console.log(getmins);
+
+    // if(getmins < 10){
+    //     // getmins = '0' +getmins
+    //     getmins = '0' + String(getmins)
+    // }
 
     let getsecs = Math.floor(getvideoscreen.currentTime%60);
     // console.log(getsecs);
+    // if(getsecs < 10){
+    //     // getsecs = '0' +getsecs;
+    //     getsecs = '0' +String(getsecs);
+    // }
 
-    if(getsecs < 10){
-        // getsecs = '0'+getsecs;
-        getsecs = '0'+String(getsecs)
+    // getdisplaytime.innerText = `${getmins}:${getsecs}`;
+
+
+
+    // Method 2 
+    // Noted: partStart(area(target length), startvalue(pad String))must be string data type 
+    const minutevalue = getmins.toString().padStart(2, '0');
+    const secondvalue = getsecs.toString().padStart(2, '0');
+
+    // console.log(minutevalue, secondvalue);
+    // console.log(typeof minutevalue, typeof secondvalue);
+
+    getdisplaytime.innerText = `${minutevalue}:${secondvalue}`;
+    
+
+
+    
+
+
+}
+
+
+// const getdoce = document.documentElement;
+
+function openfullscreen() {
+
+    if (getcontainer.requestFullscreen) {
+        getcontainer.requestFullscreen(); //standard wec
+    } else if (getdoce.mozRequestFullscreen) {
+        getcontainer.mozRequestFullscreen(); //  firefox
+    } else if (getdoce.webkitRequestFullscreen) {
+        getcontainer.webkitRequestFullscreeen(); //chrome / safari
+    } else if (getdoce.msRequestFullscreen) {
+        getcontainer.msRequestFullscreen(); //microsoft pro/id/edge
     }
 
-    getdisplaytime.innerText = `${getmn}:${getsecs}`;
+    getopnfullscreen.style.display = "none";
+    getclsfullscreen.style.display = "inline-block";
+
 }
 
-function setprogress(){
-    console.log('hay');
-}
+function closefullscreen() {
 
-const getdoce = document.documentElement;
-
-function openfullscreen(){
-    if(getdoce.exitFullscreen){
-        getcontainer.requestFullscreen();//standard w3c
-    }else if(getcontainer.mozRequestFullscreen){
-        getcontainer.mozRequestFullscreen();//standard w3c
-    }else if(getcontainer.webkitRequestFullscreen){
-        getcontainer.webkitRequestFullscreen();//standard w3c
-    }else if(getcontainer.msRequestFullscreen){
-        getcontainer.msRequestFullscreen(); // microsoft pro / id / edge
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullscreen) {
+        document.mozCancelFullscreen();
+    } else if (document.webkitExitFullscreeen) {
+        document.webkitExitFullscreeen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
     }
 
-    getopnfullscreeen.style.display = 'none';
-    getclsfullscreen.style.display = 'inline-block';
+
+    getopnfullscreen.style.display = "inline-block";
+    getclsfullscreen.style.display = "none";
+
 }
 
-function closefullscreen(){
-    if(document.exitFullscreen){
-        document.exitFullscreen(); //standard w3c
-    }else if(document.webkitExitFullscreen){
-        document.webkitExitFullscreen(); //standard w3c
-    }else if(document.msExitFullscreen){
-        document.msExitFullscreen(); // microsoft pro / id /  edge
-    }
+function setprogress(e){
+    // console.log('hay');
+    // console.log((progress.value*getvideoscreen.duration)/100);
 
-    getopnfullscreeen.style.display = 'inline-block';
-    getclsfullscreen.style.display = 'none';
+	//For Range
+    // getvideoscreen.currentTime = (progress.value*getvideoscreen.duration)/100;
+
+	//For Progress Container
+	const getelewidth = this.clientWidth;
+	// console.log(getelewidth);
+
+
+	const getclickx = e.offsetX;
+	//console.log(getclickx); 
+
+	const duration = getvideoscreen.duration;
+
+	getvideoscreen.currentTime = (getclickx/getelewidth)* duration;
+	// console.log(getvideoscreen.currentTime);
+
 }
 
-function updateprogress(){
-    // console.log('hey');
-    console.log((progress.value*getvideoscreen.duration)/100);
-    getvideoscreen.currentTime =(progress.value*getvideoscreen.duration)/100
-}
 
-playbtn.addEventListener('click',playpausevdo);
-nextbtn.addEventListener('click',nextvdo);
-prevbtn.addEventListener('click',previousvdo);
 
-getvideoscreen.addEventListener('timeupdate',updateprogress)
-progress.addEventListener('click',setprogress);
+playbtn.addEventListener('click', playpausevdo);
+nextbtn.addEventListener('click', nextvdo);
+prevbtn.addEventListener('click', prevvdo);
+stopbtn.addEventListener('click', stopvideo);
 
-getfullscreen.addEventListener('click',openfullscreen);
+getvideoscreen.addEventListener('timeupdate',updateprogress);
+getvideoscreen.addEventListener('end',nextvdo);
+getvideoscreen.addEventListener('click',playpausevdo);
 
-// 20VD
+//For Rrange
 
-getopnfullscreeen.addEventListener('click',openfullscreen);
-getclsfullscreen.addEventListener('click',closefullscreen)
+// progress.addEventListener('click',setprogress);
 
-// 21VDO
+//For Progress Container
+getprogressctn.addEventListener('click',setprogress);
+
+
+
+getopnfullscreen.addEventListener('click', openfullscreen);
+getclsfullscreen.addEventListener('click', closefullscreen);
+
+
+// 27TT
