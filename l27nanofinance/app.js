9d07432d4b723a9d618a040318da48248f71379d
Adam26114 <zakato26114@gmail.com>
2023-01-31 18:34:52 +0630

    31 jan 2023

--- l27nanofinance/app.js
+++ l27nanofinance/app.js
@@ -15,25 +15,118 @@ const gethisbox = document.querySelector('.history-box');
 const getlistgroup = document.getElementById('list-group');
 
 
-const dummydatas = [
-    {id:1,transtatus:true,remark:'Petty Cash',amount:1000,date:"2023-01-20"},
-    {id:1,transtatus:false,remark:'Pen',amount:-20,date:"2023-01-20"},
-    {id:1,transtatus:true,remark:'Other Income',amount:300,date:"2023-01-20"},
-    {id:1,transtatus:false,remark:'Book',amount:-10,date:"2023-01-20"},
-    {id:1,transtatus:false,remark:'Water',amount:-150,date:"2023-01-20"},
-    {id:1,transtatus:false,remark:'Teamix',amount:-100,date:"2023-01-20"}
-];
+// const dummydatas = [
+//     {id:1,transtatus:'+',amount:1000,remark:'Petty Cash'},
+//     {id:1,transtatus:'-',amount:-20,remark:'Pen'},
+//     {id:1,transtatus:'+',amount:300,remark:'Other Income'},
+//     {id:1,transtatus:'-',amount:-10,remark:'Book'},
+//     {id:1,transtatus:'-',amount:-150,remark:'Water'},
+//     {id:1,transtatus:'-',amount:-100,remark:'Teamix'}
+// ];
 
-console.log(dummydatas);
+// console.log(dummydatas);
+
+
+const getlsdatas = JSON.parse(localStorage.getItem('transaction'));
+let gethistories = localStorage.getItem('transaction') !== null ? getlsdatas : [];
+
+
+//Initial App
+function init(){
+    getlistgroup,innerHTML = ""
+    //Method 1
+    
+    // dummydatas.forEach(function(dummydata){
+        //     // console.log(dummydata);
+        
+        //     addtoui(dummydata);
+    // });
+
+    //Method 2
+    // dummydatas.forEach(dummydata=>addtoui(dummydata));
+    
+    //Method 3 ***
+    // dummydatas.forEach(addtoui);
+
+
+    gethistories.forEach(addtoui);
+    
+
+    
+}
+
+
+init();
+
+//Create new li to ul
+function addtoui(transtion){
+    // console.log(transtion);
+    // console.log(transtion.amount , typeof transtion.amount );
+
+    const newli = document.createElement('li');
+
+    newli.innerHTML = `${transtion.remark} <span>${transtion.transtatus}</span>${Math.abs(transtion.amount)} <span>${transtion.date}</span><button type="button" class="delete-btn">&times;</button>`;
+
+    newli.className = 'list-group-item';
+
+    newli.classList.add(transtion.transtatus === "+" ? 'inc' : 'dec');
+    // console.log(newli);
+
+    getlistgroup.appendChild(newli)
+
+}
+
+
+var sign = "-" ;
+
+// get sign
+
+gettranstatues.forEach(function(gettranstatues){
+    gettranstatues.addEventListener('change',function(){
+        // console.log(this.value);
+
+        if(this.value === "debit"){
+            sign = "+";
+        }else if(this.value === "credit"){
+            sign = "-"
+        }
+    });
+});
+
+
+
+function newtransaction(e){
+    // console.log('hay');
+    // console.log(sign);
+
+    if(isNaN(getamount.value) || getamount.value.trim() === '' || getdate.value.trim() === '' || getremark.value.trim() === ''){
+        alert('Ohh!!! Some data are missing.');
+    }else{
+        
+        const transaction = {
+            id:1000,
+            transtatus:sign,
+            amount:sign ===  "-" ? Number(-getamount.value) : Number(getamount.value),
+            date:getdate.value,
+            remark:getremark.value
+        };
+
+        // console.log(transaction);
+
+
+        gethistories.push(transaction);
+
+        addtoui(transaction);
+
+    }
+
+
+    e.preventDefault();
+}
 
 openbtn.addEventListener('click',function(){
     gethisbox.classList.toggle('show');
-})
-
-function newtransaction(e){
-    console.log('hay');
-    e.preventDefault();
-}
+});
 
 getform.addEventListener('submit',newtransaction);
 // 25TR
